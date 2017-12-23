00c7b9d683f605f52ba003575759f982f913c8bd
PARITOSH <paritoshsrivastava9199@gmail.com>
2017-12-23 21:13:42 +0530

    Initial commit

--- app/src/main/java/com/android/paritosh/libryed/BooksActivity.java
+++ app/src/main/java/com/android/paritosh/libryed/BooksActivity.java
@@ -2,12 +2,43 @@ package com.android.paritosh.libryed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class BooksActivity extends AppCompatActivity {
+public class BooksActivity extends AppCompatActivity implements BooksDialogue.BooksListener {
+
+    private Button addBooks;
+    private ListView booksList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
+
+        addBooks = findViewById(R.id.AddBooks);
+        booksList = findViewById(R.id.BooksListView);
+
+        addBooks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBookAdder();
+            }
+        });
+    }
+
+    public void openBookAdder(){
+        BooksDialogue booksDialogue = new BooksDialogue();
+        booksDialogue.show(getSupportFragmentManager(), "books dialogue");
+    }
+
+    @Override
+    public void applyText(String bname, String aname, String desc) {
+        //use bname , aname, desc to get the values from the dialogue box
+        //the above variables contain the value of the book name , auth name, description
+
+        // now open the firebase database object to add it into the database
+        //create a custom adapter and a BookInformation class to pass the data into the cloud via the
+        //adapter
     }
 }
---
+++ app/src/main/java/com/android/paritosh/libryed/BooksDialogue.java
@@ -0,0 +1,70 @@
+package com.android.paritosh.libryed;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by PARITOSH on 12/23/2017.
+ */
+
+public class BooksDialogue extends AppCompatDialogFragment  {
+
+    private EditText bookName,authorName,desc;
+    private BooksListener listener;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater layoutInflater = getActivity().getLayoutInflater();
+        View view = layoutInflater.inflate(R.layout.add_books_dialogue, null);
+
+        builder.setView(view).setTitle("Add Book").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        })
+        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String bname = bookName.getText().toString();
+                String aname = authorName.getText().toString();
+                String descriptionNotes = desc.getText().toString();
+
+                listener.applyText(bname,aname,descriptionNotes);
+
+            }
+        });
+
+        bookName  = view.findViewById(R.id.bookName);
+        authorName = view.findViewById(R.id.authorName);
+        desc = view.findViewById(R.id.descrName);
+
+        return builder.create();
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (BooksListener) context;
+        }catch (Exception e){
+            throw new ClassCastException(context.toString() + "must implement the dialogue listener");
+        }
+    }
+
+
+
+    public interface BooksListener{
+        void applyText(String bname,String aname, String desc);
+    }
+}
